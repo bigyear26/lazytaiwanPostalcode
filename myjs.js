723342324a79b11e54b4000c2bc8634f81fa0c07
bigyear26 <bigyear26@gmail.com>
2017-07-31 12:34:01 +0800

    1.試試看2

---
+++ myjs.js
@@ -0,0 +1,29 @@
+
+
+
+function get_zip5_adrs(a_id) {
+
+    var word=$("#a_id").val();
+    var words=word.split("\n");
+    var wordsLength=words.length-1;
+
+    for (i = 0; i <= wordsLength; i++) {
+        var zip5_url = "https://zip5.5432.tw/zip5json.py?adrs=" + words[i] + "&jsoncallback=?"
+            $.getJSON(zip5_url,
+                function (data) {
+                    $("#target1").append(data.new_adrs+","+data.adrs + "\n");
+                    setTimeout("gogo(data)",100000)
+                }
+
+            );
+        }
+    }
+
+function refresh() {
+    window.location.reload();
+}
+    //刷新当前页面.
+function total() {
+    $("#count").text($("#a_id").val().split("\n").length)
+}
+
