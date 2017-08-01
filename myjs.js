0d150d2017801485b960cf343d1b2fb29bd62ec4
bigyear26 <bigyear26@gmail.com>
2017-08-01 09:32:11 +0800

    1.setTimeout 1.5 sec 2.layout beautify 3.GAtag added

--- myjs.js
+++ myjs.js
@@ -1,26 +1,6 @@
 
 
 
-function get_zip5_adrs(a_id) {
-
-    var word=$("#a_id").val();
-    var words=word.split("\n");
-    var wordsLength=words.length-1;
-
-    for (var i = 0; i <= wordsLength; i++) {
-        (function(i) { // protects i in an immediately called function
-            $.getJSON("https://zip5.5432.tw/zip5json.py?adrs=" + words[i] + "&jsoncallback=?",
-                function (data){
-                    function run() {
-                        $("#target1").append(data.new_adrs+","+data.adrs + "\n");
-                    };
-                setTimeout(run,4000*i);
-                });
-        })(i);
-    }
-
-
-}
 
 function refresh() {
     window.location.reload();
@@ -35,4 +15,25 @@ function gao() {
     var words=word.split("\n");
     var wordsLength=words.length-1;
     alert(words[1])
+}
+
+
+function get_zip5_adrs(a_id) {
+
+    var word=$("#a_id").val();
+    var words=word.split("\n");
+    var wordsLength=words.length-1;
+    for (var i = 0; i <= wordsLength; i++) {
+        (function(i) { // protects i in an immediately called function
+            $.getJSON("https://zip5.5432.tw/zip5json.py?adrs=" + words[i] + "&jsoncallback=?",
+                function (data){
+                    function run() {
+                        $("#target1").append(data.new_adrs+","+data.adrs + "\n");
+                    };
+                    setTimeout(run,1500*i);
+                });
+        })(i);
+    }
+
+
 }
