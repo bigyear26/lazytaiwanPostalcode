fbbc3cf2b1ca8596e5e256098879d88dc90d09ba
bigyear26 <bigyear26@gmail.com>
2017-09-22 13:56:01 +0800

    1.增加第二功能v2

--- myjs.js
+++ myjs.js
@@ -107,3 +107,90 @@ function get_zip5_adrsSplitAd(a_id) {
 
 
 }
+
+
+// 第二功能
+
+
+var i;
+var split;
+
+function trans() {
+    i = 0;
+    $("#target").val("");
+    var content = $("#source").val();
+    split = content.split("\n");
+    delayedLoop();
+}
+
+function delayedLoop() {
+    addressToLatLng(split[i]);
+    if (++i == split.length) {
+        return;
+    }
+    window.setTimeout(delayedLoop, 1000);
+}
+
+
+function trans1() {
+    i = 0;
+    $("#target1").val("");
+    var content = $("#source").val();
+    split = content.split("\n");
+    delayedLoop1();
+}
+
+function delayedLoop1() {
+    addressToLatLng1(split[i]);
+    if (++i == split.length) {
+        return;
+    }
+    window.setTimeout(delayedLoop1, 1000);
+}
+
+
+
+
+
+function addressToLatLng(addr) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+        "address": addr
+    }, function (results, status) {
+        if ($("#c").attr('checked'))
+        {
+            addr = addr + ",";
+        }
+        else {
+            addr = "";
+        }
+        if (status == google.maps.GeocoderStatus.OK) {
+            var content = $("#target03").val();
+            $("#target03").val(content + addr + results[0].geometry.location.lng() + "\n");
+        } else {
+            var content = $("#target03").val();
+            $("#target03").val(content + addr + "查無經度" + "\n");
+        }
+    });
+}
+function addressToLatLng1(addr) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+        "address": addr
+    }, function (results, status) {
+        if ($("#c").attr('checked'))
+        {
+            addr = addr + ",";
+        }
+        else {
+            addr = "";
+        }
+        if (status == google.maps.GeocoderStatus.OK) {
+            var content = $("#target04").val();
+            $("#target04").val(content + addr + results[0].geometry.location.lat() + "\n");
+        } else {
+            var content = $("#target04").val();
+            $("#target04").val(content + addr + "查無緯度" + "\n");
+        }
+    });
+}
